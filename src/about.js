c9224547949c7219b248d253f5de3abe05b5a190
RJ Ocampo <rjqocampo@gmail.com>
2023-09-05 00:51:59 +0800

    Finish hero section

--- src/about.js
+++ src/about.js
@@ -8,15 +8,18 @@ export default function showLanding() {
   sectionHero.classList.add('hero')
   div.appendChild(sectionHero);
 
+  const divHero = document.createElement('div');
+  sectionHero.appendChild(divHero);
+
   const h1 = document.createElement('h1');
-  h1.textContent = 'Hero';
-  sectionHero.appendChild(h1);
+  h1.textContent = 'SAORI';
+  divHero.appendChild(h1);
 
   const h2 = document.createElement('h2');
   h2.textContent = 'モダンな日本料理';
-  sectionHero.appendChild(h2);
+  divHero.appendChild(h2);
 
   const vl = document.createElement('div');
   vl.classList.add('vertical-line');
-  sectionHero.appendChild(vl);
+  divHero.appendChild(vl);
 }
